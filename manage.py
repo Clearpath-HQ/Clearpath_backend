4ed0a624b96e1678b7b7af62dd447d333ec0664e
Paulo Ouana <paulocouana.pauloc.ouana@gmail.com>
2024-05-20 02:07:24 +0000

    refactor: improve code structure

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clearpath_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clearpath_home.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
